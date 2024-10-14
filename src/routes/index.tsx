2d6f3b92946853bce96840c8e059e6e271076eee
Dryspell <Nhwinslow@gmail.com>
2024-10-14 01:18:17 -0500

    Added Drizzle and PicoCSS

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,23 @@
-import { Title } from "@solidjs/meta";
+import { createAsync, type RouteDefinition } from "@solidjs/router";
+import { getUser, logout } from "./api";
+
+export const route = {
+  preload() {
+    getUser();
+  },
+} satisfies RouteDefinition;
 
 export default function Home() {
+  const user = createAsync(async () => getUser(), { deferStream: true });
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+    <main class="w-full space-y-2 p-4">
+      <h2 class="text-3xl font-bold">Hello {user()?.username}</h2>
+      <h3 class="text-xl font-bold">Message board</h3>
+      <form action={logout} method="post">
+        <button name="logout" type="submit">
+          Logout
+        </button>
+      </form>
     </main>
   );
 }
