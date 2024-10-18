d73b11f178c34e0d4f42f0af47f4406b1cb11b8d
Dryspell <Nhwinslow@gmail.com>
2024-10-18 12:41:59 -0500

    Refactor canvas game to use createStore for multiple objects

--- src/routes/canvas.tsx
+++ src/routes/canvas.tsx
@@ -1,6 +1,8 @@
 import { Accessor, createSignal, onMount, Setter } from "solid-js";
+import { createStore, SetStoreFunction } from "solid-js/store";
 import { Button } from "~/components/ui/button";
 import { Progress } from "~/components/ui/progress";
+
 const createRect = (
   ctx: CanvasRenderingContext2D,
   x: number,
@@ -15,8 +17,8 @@ const createRect = (
 
 const initializeGame = (
   gameCanvas: HTMLCanvasElement | undefined,
-  rect: Accessor<Rect>,
-  setRect: Setter<Rect>,
+  gameObjects: typeof DEFAULT_OBJECTS,
+  setGameObjects: SetStoreFunction<typeof DEFAULT_OBJECTS>,
 ) => {
   if (!gameCanvas) {
     console.error("Canvas element is not defined");
@@ -27,35 +29,33 @@ const initializeGame = (
     console.error("Failed to get 2d context from canvas");
     return;
   }
-
-  const [velocity, setVelocity] = createSignal([1, 1] as [dx: number, dy: number]);
   const canvasSize = [gameCanvas.width, gameCanvas.height] as [width: number, height: number];
 
-  const gameLoop = () => {
-    ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+  const dvdBounceGameLoop = () => {
+    ctx.clearRect(0, 0, ...canvasSize);
 
-    setRect(
-      rect().map((coord, i) => {
-        if ([0, 1].includes(i)) {
-          if (coord + velocity()[i] > canvasSize[i] - rect()[2 + i] || coord + velocity()[i] < 0) {
-            setVelocity(velocity().map((v, j) => (i === j ? -v : v)) as [number, number]);
+    setGameObjects("players", [0, gameObjects.players.length - 1], player => {
+      return {
+        ...player,
+        pos: player.pos.map((coord, i) => {
+          if (
+            coord + player.velocity[i] > canvasSize[i] - player.dims[i] ||
+            coord + player.velocity[i] < 0
+          ) {
+            player.velocity[i] = -player.velocity[i];
           }
-          return coord + velocity()[i];
-        } else {
-          return coord;
-        }
-      }) as [number, number, number, number],
-    );
+          return coord + player.velocity[i];
+        }) as Player["pos"],
+      };
+    });
 
-    createRect(ctx, ...rect());
+    gameObjects.players.forEach(player => createRect(ctx, ...player.pos, ...player.dims));
 
-    requestAnimationFrame(gameLoop);
+    requestAnimationFrame(dvdBounceGameLoop);
   };
 
-  gameLoop();
+  dvdBounceGameLoop();
 };
-
-type Rect = [x: number, y: number, width: number, height: number];
 
 const getPagePosition = (canvas: HTMLCanvasElement | undefined, posX: number, posY: number) => {
   if (!canvas) {
@@ -68,16 +68,20 @@ const getPagePosition = (canvas: HTMLCanvasElement | undefined, posX: number, po
   return [x, y] as [x: number, y: number];
 };
 
-const FollowingUI = (props: { pos: [x: number, y: number] }) => {
+const FollowingUI = (props: {
+  gameCanvas: Accessor<HTMLCanvasElement | undefined>;
+  player: Player;
+}) => {
+  const pos = () => getPagePosition(props.gameCanvas(), ...props.player.pos);
   return (
-    <div style={{ position: "absolute", left: `${props.pos[0]}px`, top: `${props.pos[1]}px` }}>
-      <Button>Click</Button>
+    <div style={{ position: "absolute", left: `${pos()[0]}px`, top: `${pos()[1]}px` }}>
+      <Button>{props.player.id}</Button>
       <Progress
-        value={80}
+        value={props.player.hp}
         minValue={0}
-        maxValue={100}
+        maxValue={props.player.maxHp}
         style={{
-          width: `${100}px`,
+          width: `${props.player.dims[0]}px`,
         }}
         color="bg-red-500"
       />
@@ -85,28 +89,80 @@ const FollowingUI = (props: { pos: [x: number, y: number] }) => {
   );
 };
 
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 600;
+
+const DEFAULT_OBJECTS = {
+  players: [
+    {
+      type: "player",
+      id: "player1",
+      color: "red",
+      pos: [150, 300],
+      dims: [100, 100],
+      velocity: [1, 1],
+      hp: 80,
+      maxHp: 100,
+    },
+    {
+      type: "player",
+      id: "player2",
+      color: "blue",
+      pos: [650, 300],
+      dims: [100, 100],
+      velocity: [1, 1],
+      hp: 80,
+      maxHp: 100,
+    },
+  ] as Player[],
+} as const;
+
+type Player = {
+  id: string;
+  type: "player";
+  color: string;
+  pos: [x: number, y: number];
+  dims: [width: number, height: number];
+  velocity: [dx: number, dy: number];
+  hp: number;
+  maxHp: number;
+};
+
+type Enemy = {
+  type: "enemy";
+  color: string;
+  pos: [x: number, y: number];
+  dims: [width: number, height: number];
+  hp: number;
+  maxHp: number;
+};
+
+type GameObject = Player | Enemy;
+
 export default function Game() {
   const [gameCanvas, setGameCanvas] = createSignal<HTMLCanvasElement | undefined>(undefined);
-  const [rect, setRect] = createSignal([10, 10, 100, 100] as Rect);
-  const pos = () => getPagePosition(gameCanvas(), rect()[0], rect()[1]);
+  const [gameObjects, setGameObjects] = createStore(DEFAULT_OBJECTS);
 
   onMount(() => {
-    gameCanvas() && initializeGame(gameCanvas(), rect, setRect);
+    gameCanvas() && initializeGame(gameCanvas(), gameObjects, setGameObjects);
+    // console.log(gameObjects.players);
   });
 
   return (
     <main class="relative mx-auto p-4 text-gray-700">
-      <FollowingUI pos={pos()} />
+      {gameObjects.players.map((player: Player) => (
+        <FollowingUI player={player} gameCanvas={gameCanvas} />
+      ))}
       <canvas
         ref={setGameCanvas}
-        width={800}
-        height={600}
+        width={CANVAS_WIDTH}
+        height={CANVAS_HEIGHT}
         style={{
           "justify-content": "center",
           "align-items": "center",
           position: "relative",
-          width: "800",
-          height: "600",
+          width: `${CANVAS_WIDTH}px`,
+          height: `${CANVAS_HEIGHT}px`,
           border: "1px solid black",
         }}
       />
