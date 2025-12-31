5e283b831744af457fd6d32aae1fe4b0fde8de05
Dryspell <Nhwinslow@gmail.com>
2025-12-31 17:04:28 -0600

    cleanup socketio code

--- src/lib/Client/socket.ts
+++
@@ -1,3 +0,0 @@
-// socket.io client is deprecated in favor of SpacetimeDB subscriptions.
-// This file remains to avoid import breakage if referenced; export a noop.
-export const socket = undefined as unknown as never;
